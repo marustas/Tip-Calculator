3ca8fe58a3b06a65f30c1a9376acaa5c9e15be14
Stanislau Marudau <marudov.stas@gmail.com>
2023-08-08 19:21:59 +0300

    Labels added for tip selection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 
-const Bill = ({ onSetBill }) => {
+const Bill = ({ onSetBill, bill }) => {
   return (
     <div>
       <p>How much was the bil??</p>
       <input
+        value={bill}
         onChange={(e) => onSetBill(Number(e.target.value))}
         type="text"
       ></input>
@@ -30,27 +31,11 @@ const ResetBtn = ({ onSetBill, onSetTip, onSetTipFriend }) => {
   return <button onClick={(e) => reset()}>Reset</button>;
 };
 
-const Tip = ({ tip, onSetTip }) => {
+const Tip = ({ children, tip, onSetTip }) => {
   return (
     <div>
-      <p>How did you like the service?</p>
+      <label>{children}</label>
       <select value={tip} onChange={(event) => onSetTip(event.target.value)}>
-        <option value="0">Dissatisfied 0%</option>
-        <option value="5">It was okay 5%</option>
-        <option value="10">It was good 10%</option>
-        <option value="20">It was wonderful 20%</option>
-      </select>
-    </div>
-  );
-};
-const TipFriend = ({ tipFriend, onSetTipFriend }) => {
-  return (
-    <div>
-      <p>How did your friend like the service?</p>
-      <select
-        value={tipFriend}
-        onChange={(event) => onSetTipFriend(event.target.value)}
-      >
         <option value="0">Dissatisfied 0%</option>
         <option value="5">It was okay 5%</option>
         <option value="10">It was good 10%</option>
@@ -63,21 +48,25 @@ const TipFriend = ({ tipFriend, onSetTipFriend }) => {
 const App = () => {
   const [bill, setBill] = useState(0);
   const [tip, setTip] = useState("0");
-  const [tipFriend, setTipFriend] = useState("0");
+  const [tipFriend, SetTipFriend] = useState("0");
   let cost;
   cost = ((Number(tip) + Number(tipFriend)) / 200) * bill + bill;
   return (
     <div className="App">
-      <Bill onSetBill={setBill} />
-      <Tip tip={tip} onSetTip={setTip} />
-      <TipFriend tipFriend={tipFriend} onSetTipFriend={setTipFriend} />
+      <Bill bill={bill} onSetBill={setBill} />
+      <Tip tip={tip} onSetTip={setTip}>
+        How did you like the service?
+      </Tip>
+      <Tip tipFriend={tip} onSetTip={SetTipFriend}>
+        How did your friend like the service?
+      </Tip>
       {bill > 0 ? (
         <div>
           <Total cost={cost} bill={bill} />
           <ResetBtn
             onSetBill={setBill}
             onSetTip={setTip}
-            onSetTipFriend={setTipFriend}
+            onSetTipFriend={SetTipFriend}
           />
         </div>
       ) : (
